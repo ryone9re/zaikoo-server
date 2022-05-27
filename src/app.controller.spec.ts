15883f422a4206b52160689f836080a59fdf97b6
ryone9re <ryoichi.kurimoto@gmail.com>
2022-05-27 18:03:18 +0000

    設定テスト

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing'
+
 import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { PrismaService } from './prisma.service'
 
 describe('AppController', () => {
   let appController: AppController
@@ -8,15 +9,22 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [PrismaService],
     }).compile()
 
     appController = app.get<AppController>(AppController)
   })
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!')
+    it('should return "Hello World!"', async () => {
+      const data = await appController.getAllUsers()
+      expect(data).toStrictEqual([
+        {
+          id: 1,
+          email: 'mail@mail.mail',
+          name: 'Hello',
+        },
+      ])
     })
   })
 })
