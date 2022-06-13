0446f7d07cda1b1fbe5c9740b6e72224d1dfe46c
ryone9re <ryoichi.kurimoto@gmail.com>
2022-06-13 12:47:25 +0000

    Initialize project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common'
 
-import { AppController } from './app.controller'
 import { PrismaService } from './prisma.service'
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [],
   providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class AppModule {}
