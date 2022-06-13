efd6f964cbe3b12f197095b90e306470d05756fe
ryone9re <ryoichi.kurimoto@gmail.com>
2022-06-13 18:54:11 +0000

    PrismaServiceを実際に使うところへ移動

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { PrismaService } from './../prisma.service';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 
 @Module({
   controllers: [ProductController],
-  providers: [ProductService],
+  providers: [ProductService, PrismaService],
 })
 export class ProductModule {}
