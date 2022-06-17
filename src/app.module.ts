3215cb34f431f1c2900e94f22082f4908f7659cd
ryone9re <ryoichi.kurimoto@gmail.com>
2022-06-17 19:10:33 +0000

    taxRatesモジュール作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoryModule } from './category/category.module';
 import { ProductModule } from './product/product.module';
+import { TaxRatesModule } from './tax-rates/tax-rates.module';
 
 @Module({
-  imports: [ProductModule, CategoryModule],
+  imports: [ProductModule, CategoryModule, TaxRatesModule],
   controllers: [AppController],
   providers: [AppService],
 })
