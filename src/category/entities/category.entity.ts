991fd4400d4e5d59a2ec4374f71fbfed26f50edd
ryone9re <ryoichi.kurimoto@gmail.com>
2022-06-18 18:51:41 +0000

    エンティティを実装

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,1 +1,9 @@
-export class Category {}
+import { Child_category } from '@prisma/client';
+
+export class Category implements Child_category {
+  id: number;
+  parent_id: number;
+  name: string;
+  created_at: Date;
+  updated_at: Date;
+}
