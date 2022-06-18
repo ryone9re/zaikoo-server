012996edf7fe4cad64826a8428a995da7a2ce847
ryone9re <ryoichi.kurimoto@gmail.com>
2022-06-18 20:28:04 +0000

    エンティティ作成

--- src/office/supplier/entities/supplier.entity.ts
+++ src/office/supplier/entities/supplier.entity.ts
@@ -1,1 +1,14 @@
-export class Supplier {}
+import { Supplier as SupplierType } from '@prisma/client';
+
+export class Supplier implements SupplierType {
+  id: number;
+  supplier_name: string;
+  postal_code: string;
+  address: string;
+  email_address: string;
+  phone_number: string;
+  division_name: string | null;
+  responsible_name: string | null;
+  created_at: Date;
+  updated_at: Date;
+}
