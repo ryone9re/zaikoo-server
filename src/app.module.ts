a18ea93064a8e9c0f22e7faeb7615f294407b7fa
ryone9re <ryoichi.kurimoto@gmail.com>
2022-06-18 20:19:22 +0000

    officeモジュール追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoryModule } from './category/category.module';
+import { OfficeModule } from './office/office.module';
 import { ProductModule } from './product/product.module';
 import { TaxRatesModule } from './tax-rates/tax-rates.module';
 
 @Module({
-  imports: [ProductModule, CategoryModule, TaxRatesModule],
+  imports: [ProductModule, CategoryModule, TaxRatesModule, OfficeModule],
   controllers: [AppController],
   providers: [AppService],
 })
