1d281c2e76612babf0bdbea9e7998a482a70d8bc
ryone9re <ryoichi.kurimoto@gmail.com>
2022-06-18 20:27:59 +0000

    dto作成

--- src/office/supplier/dto/create-supplier.dto.ts
+++ src/office/supplier/dto/create-supplier.dto.ts
@@ -1,1 +1,9 @@
-export class CreateSupplierDto {}
+export class CreateSupplierDto {
+  supplier_name: string;
+  postal_code: string;
+  address: string;
+  email_address: string;
+  phone_number: string;
+  division_name?: string;
+  responsible_name?: string;
+}
