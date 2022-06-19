3cbdbeed4c564e612d7a1193659f82663b040cb2
ryone9re <ryoichi.kurimoto@gmail.com>
2022-06-19 15:07:28 +0000

    バリデーション追加

--- src/office/base/dto/create-base.dto.ts
+++ src/office/base/dto/create-base.dto.ts
@@ -1,23 +1,35 @@
-import { IsEmail, IsNotEmpty, Length } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumberString,
+  IsString,
+  Length,
+} from 'class-validator';
 
 export class CreateBaseDto {
   @IsNotEmpty()
+  @IsString()
   base_name: string;
 
   @IsNotEmpty()
+  @IsNumberString()
   @Length(7, 7)
   postal_code: string;
 
   @IsNotEmpty()
+  @IsString()
   address: string;
 
+  @IsNumberString()
   @Length(0, 15)
   phone_number?: string;
 
   @IsEmail()
   email_address?: string;
 
+  @IsString()
   division_name?: string;
 
+  @IsString()
   responsible_name?: string;
 }
--- src/office/supplier/dto/create-supplier.dto.ts
+++ src/office/supplier/dto/create-supplier.dto.ts
@@ -1,14 +1,23 @@
-import { IsEmail, IsNotEmpty, Length } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumberString,
+  IsString,
+  Length,
+} from 'class-validator';
 
 export class CreateSupplierDto {
   @IsNotEmpty()
+  @IsString()
   supplier_name: string;
 
   @IsNotEmpty()
+  @IsNumberString()
   @Length(7, 7)
   postal_code: string;
 
   @IsNotEmpty()
+  @IsString()
   address: string;
 
   @IsNotEmpty()
@@ -16,10 +25,13 @@ export class CreateSupplierDto {
   email_address: string;
 
   @IsNotEmpty()
+  @IsNumberString()
   @Length(0, 15)
   phone_number: string;
 
+  @IsString()
   division_name?: string;
 
+  @IsString()
   responsible_name?: string;
 }
