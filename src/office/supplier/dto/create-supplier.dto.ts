ded00d26954233dc079c6b55a8a857d9035e1efe
ryone9re <ryoichi.kurimoto@gmail.com>
2022-06-19 10:33:59 +0000

    バリデーション追加

--- src/office/supplier/dto/create-supplier.dto.ts
+++ src/office/supplier/dto/create-supplier.dto.ts
@@ -1,9 +1,20 @@
+import { IsEmail, Length } from 'class-validator';
+
 export class CreateSupplierDto {
   supplier_name: string;
+
+  @Length(7, 7)
   postal_code: string;
+
   address: string;
+
+  @IsEmail()
   email_address: string;
+
+  @Length(0, 15)
   phone_number: string;
+
   division_name?: string;
+
   responsible_name?: string;
 }
