6a55d82373078fe3718ee4d9d0dd26fd462e9c3e
ryone9re <ryoichi.kurimoto@gmail.com>
2022-06-19 11:01:41 +0000

    DTO作成

--- src/office/base/dto/create-base.dto.ts
+++ src/office/base/dto/create-base.dto.ts
@@ -1,1 +1,23 @@
-export class CreateBaseDto {}
+import { IsEmail, IsNotEmpty, Length } from 'class-validator';
+
+export class CreateBaseDto {
+  @IsNotEmpty()
+  base_name: string;
+
+  @IsNotEmpty()
+  @Length(7, 7)
+  postal_code: string;
+
+  @IsNotEmpty()
+  address: string;
+
+  @Length(0, 15)
+  phone_number?: string;
+
+  @IsEmail()
+  email_address?: string;
+
+  division_name?: string;
+
+  responsible_name?: string;
+}
