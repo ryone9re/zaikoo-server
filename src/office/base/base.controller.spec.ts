1bb91a5748a8e37d00182a273bffaa8337286ea8
ryone9re <ryoichi.kurimoto@gmail.com>
2022-06-19 11:12:48 +0000

    Prismaサービス削除

--- src/office/base/base.controller.spec.ts
+++ src/office/base/base.controller.spec.ts
@@ -1,23 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
-import { PrismaService } from './../../prisma.service';
 import { BaseController } from './base.controller';
 import { BaseService } from './base.service';
 
 describe('BaseController', () => {
   let controller: BaseController;
-  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BaseController],
-      providers: [BaseService, PrismaService],
+      providers: [BaseService],
     }).compile();
 
     controller = module.get<BaseController>(BaseController);
-    prisma = module.get<PrismaService>(PrismaService);
-
-    await prisma.base.deleteMany({});
   });
 
   it('should be defined', () => {
