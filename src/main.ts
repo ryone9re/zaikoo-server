9a39b14bfca0f8462a50a0a8f14391c8914a4274
ryone9re <ryoichi.kurimoto@gmail.com>
2022-06-19 10:33:42 +0000

    Prismaをリッスン前に呼ぶよう変更

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ import { PrismaService } from './prisma.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
+  await app.listen(3000);
 }
 bootstrap();
