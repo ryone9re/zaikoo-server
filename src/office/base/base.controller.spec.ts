710c73e3279bdd46c90f300cca9e77e25729d309
ryone9re <ryoichi.kurimoto@gmail.com>
2022-06-19 11:01:58 +0000

    テストにPrisma注入

--- src/office/base/base.controller.spec.ts
+++ src/office/base/base.controller.spec.ts
@@ -1,18 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { PrismaService } from './../../prisma.service';
 import { BaseController } from './base.controller';
 import { BaseService } from './base.service';
 
 describe('BaseController', () => {
   let controller: BaseController;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BaseController],
-      providers: [BaseService],
+      providers: [BaseService, PrismaService],
     }).compile();
 
     controller = module.get<BaseController>(BaseController);
+    prisma = module.get<PrismaService>(PrismaService);
+
+    await prisma.base.deleteMany({});
   });
 
   it('should be defined', () => {
