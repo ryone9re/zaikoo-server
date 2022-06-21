0e86a3115e3b3c6333612aec9fc3f7c567c0da00
ryone9re <ryoichi.kurimoto@gmail.com>
2022-06-21 12:32:45 +0000

    MenuServiceの依存性を注入

--- src/stock/stock.controller.spec.ts
+++ src/stock/stock.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { MenuService } from './../menu/menu.service';
 import { PrismaService } from './../prisma.service';
 import { StockController } from './stock.controller';
 import { StockService } from './stock.service';
@@ -10,7 +11,7 @@ describe('StockController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [StockController],
-      providers: [StockService, PrismaService],
+      providers: [StockService, PrismaService, MenuService],
     }).compile();
 
     controller = module.get<StockController>(StockController);
--- src/stock/stock.module.ts
+++ src/stock/stock.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { MenuService } from './../menu/menu.service';
 import { PrismaService } from './../prisma.service';
 import { StockController } from './stock.controller';
 import { StockService } from './stock.service';
 
 @Module({
   controllers: [StockController],
-  providers: [StockService, PrismaService],
+  providers: [StockService, PrismaService, MenuService],
 })
 export class StockModule {}
